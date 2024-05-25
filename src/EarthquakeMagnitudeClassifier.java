b56ba3bf08e885dc4fb20c430df9de4cca1e7efb
faruk-ayrcc25 <you@example.com>
2024-05-25 18:33:04 +0300

    Scanner defined.

--- src/EarthquakeMagnitudeClassifier.java
+++ src/EarthquakeMagnitudeClassifier.java
@@ -1,7 +1,19 @@
 package src;
 
+import java.util.Scanner;
+
 public class EarthquakeMagnitudeClassifier {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the magnitude of the earthquake: ");
+        double magnitude = scanner.nextDouble();
+
+        String classifier = findClassifier(magnitude);
+        System.out.println(classifier);
     }
+
+
 }
+
